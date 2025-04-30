8f4b07d642b5c434633fb05ac2bcf7195f7d32ea
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-30 16:08:37 -0400

    initial commit.

---
+++ index.js
@@ -0,0 +1,36 @@
+// 
+import express from "express";
+// allows us to parse through the body of the request
+import bodyParser from "body-parser";
+
+// postgreSQL module
+import pg from "pg";
+
+// allows us to access our passwords and other sensitive variables from the .env file
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+// allows the application to use EJS
+app.set('view engine', 'ejs');
+
+// allows the Express server to correctly read form data via the body of the request (req.body)
+app.use(bodyParser.urlencoded({ extended: true }));
+// tells the application to serve static files (CSS & other front end files) from the "public" directory
+app.use(express.static("public"));
+
+// default GET route
+app.get("/", async (req, res) => {
+    console.log(`Default GET route (\'/\'): req.body = `, req.body);
+    // render the idex.ejs file
+    res.render("index", {
+
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
